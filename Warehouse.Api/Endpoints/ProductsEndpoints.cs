ee423355f04ec191233cb1f950b8f5419ba913c3
Martin Zbojovský <martinzbojovsky@MacBook-Air-uzivatela-Martin.local>
2026-04-19 11:59:20 +0200

    separated endpoints to own file + group routes

---
+++ Warehouse.Api/Endpoints/ProductsEndpoints.cs
@@ -0,0 +1,78 @@
+using System;
+using Warehouse.Api.Dtos;
+
+namespace Warehouse.Api.Endpoints;
+
+public static class ProductsEndpoints
+{
+    const string GetProductEndpoint = "GetProduct";
+    private static readonly List<ProductDto> products = [
+        new (
+            1,
+            "apple",
+            2.35M,
+            4
+        ),
+        new (
+            2,
+            "banana",
+            3.49M,
+            10
+        ),
+        new (
+            2,
+            "kiwi",
+            3.11M,
+            3
+        ),
+    ];
+
+    public static void MapProductsEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/products");
+
+        // GET /products 
+        group.MapGet("/", () => products );
+
+        // GET /products/{id} 
+        group.MapGet("/{id}", (int id) => {
+            var product = products.Find(product => product.Id == id);
+
+            return product is null ? Results.NotFound() : Results.Ok(product);
+        })
+        .WithName(GetProductEndpoint);
+
+        // POST /products 
+        group.MapPost("/", (CreateProductDto newProduct) =>
+        {
+            ProductDto product = new (
+                products.Count + 1,
+                newProduct.Name,
+                newProduct.Price,
+                newProduct.StockQuantity
+            );
+            products.Add(product);
+
+            return Results.CreatedAtRoute(GetProductEndpoint, new {id = product.Id}, product);
+
+        });
+
+        // PUT /products/{id} 
+        group.MapPut("/{id}", (int id, UpdatedProductDto updatedProduct) =>
+        {
+            int index = products.FindIndex(product => product.Id == id);
+
+            if (index == -1) return Results.NotFound();
+
+            products[index] = new ProductDto (
+                id,
+                updatedProduct.Name,
+                updatedProduct.Price,
+                updatedProduct.StockQuantity
+            );
+
+            return Results.NoContent();
+
+        });
+    }
+}
--- Warehouse.Api/Program.cs
+++ Warehouse.Api/Program.cs
@@ -1,68 +1,9 @@
 using Warehouse.Api.Dtos;
-
-const string GetProductEndpoint = "GetProduct";
+using Warehouse.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "default");
-
-List<ProductDto> products = [
-    new (
-        1,
-        "apple",
-        2.35M,
-        4
-    ),
-    new (
-        2,
-        "banana",
-        3.49M,
-        10
-    ),
-    new (
-        2,
-        "kiwi",
-        3.11M,
-        3
-    ),
-];
-
-// GET /products #get all products
-app.MapGet("/products", () => products );
-
-// GET /products/{id} #get specific product
-app.MapGet("/products/{id}", (int id) => products.Find(product => product.Id == id))
-    .WithName(GetProductEndpoint);
-
-// POST /products #create new product
-app.MapPost("/products", (CreateProductDto newProduct) =>
-{
-    ProductDto product = new (
-        products.Count + 1,
-        newProduct.Name,
-        newProduct.Price,
-        newProduct.StockQuantity
-    );
-    products.Add(product);
-
-    return Results.CreatedAtRoute(GetProductEndpoint, new {id = product.Id}, product);
-
-});
-
-// PUT /products/{id} #updates product
-app.MapPut("/products/{id}", (int id, UpdatedProductDto updatedProduct) =>
-{
-    int index = products.FindIndex(product => product.Id == id);
-    products[index] = new ProductDto (
-        id,
-        updatedProduct.Name,
-        updatedProduct.Price,
-        updatedProduct.StockQuantity
-    );
-
-    return Results.NoContent();
-
-});
+app.MapProductsEndpoints();
 
 app.Run();
